b9a4d7fbd9454eb9d6963f2cddcbd548caf1ebe2
Andrew <arcabezudo@dons.usfca.edu>
2022-03-30 20:12:42 -0700

    fixing errors

---
+++ README.md
@@ -0,0 +1,12 @@
+## Personal Blog
+
+live deployment: https://salty-depths-32871.herokuapp.com/
+
+A blog website for user to store entries into database. 
+user can venture to ``` /compose ``` to create new entries.
+
+### Tech Stack: 
+MERN w/ mongoose.
+
+database server hosted on mongodb atlas aws
+frontend server hosted via Heroku
---
+++ app.js
@@ -0,0 +1,96 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const pw = process.env.HERO_PW;
+const user = process.env.HERO_USER;
+const cluster = process.env.HERO_CLUSTER;
+
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+main().catch(err => console.log(err));
+async function main() {
+ await mongoose.connect('mongodb+srv://'+user+':'+pw+'@'+cluster+'.mongodb.net/blogDB');
+}
+
+const postSchema = new mongoose.mongoose.Schema({
+  title: String,
+  body: String
+});
+const Post = new mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+  
+  Post.find({}, function(err, foundPosts){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {startingContent: homeStartingContent, posts: foundPosts})
+    }
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.get("/posts/:postId", function(req, res){
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, foundPost) {
+    if (err) {
+      console.log("err");
+    } else {
+      res.render("post", {title: foundPost.title, content: foundPost.body});
+    }
+  });
+
+});
+
+app.post("/compose", function(req, res){
+  
+  const post = new Post ({
+    title: req.body.postTitle,
+    body: req.body.postBody
+  });
+  post.save(function(err){
+    if (err) {
+      console.log("error saving post");
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
+});
